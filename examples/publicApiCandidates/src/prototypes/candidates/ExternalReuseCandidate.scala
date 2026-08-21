75f3a65911a1cb03bd87afb6d8db22f7ddfcf8ff
pysolvesemi <pysolvesemi@gmail.com>
2026-08-21 10:01:11 +0530

    fix(api): align external reuse port names

--- examples/publicApiCandidates/src/prototypes/candidates/ExternalReuseCandidate.scala
+++ examples/publicApiCandidates/src/prototypes/candidates/ExternalReuseCandidate.scala
@@ -4,13 +4,13 @@ import external.reuse.GainStage
 import nodal.*
 
 final class ExternalReuseCandidate extends Module:
-  val input = inout(Electrical)
-  val output = inout(Electrical)
+  val source = inout(Electrical)
+  val sink = inout(Electrical)
   val common = inout(Electrical)
   val gain = param(4.0.real)
 
   private val stage = instance(new GainStage()).param(_.gain, gain)
 
-  connect(input, stage(_.input))
-  connect(output, stage(_.output))
+  connect(source, stage(_.source))
+  connect(sink, stage(_.sink))
   connect(common, stage(_.common))

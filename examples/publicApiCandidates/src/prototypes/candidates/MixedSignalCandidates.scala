6a9680bd21fa420f1074f7195fd783dd2cf37816
pysolvesemi <pysolvesemi@gmail.com>
2026-08-21 10:41:33 +0530

    examples: freeze parameterized AMS width and override forms

--- examples/publicApiCandidates/src/prototypes/candidates/MixedSignalCandidates.scala
+++ examples/publicApiCandidates/src/prototypes/candidates/MixedSignalCandidates.scala
@@ -2,7 +2,8 @@ package prototypes.candidates
 
 import nodal.*
 
-final class Adc(val width: Int = 12) extends Module:
+final class Adc extends Module:
+  val width = param(12.integer)
   val analogInput = input(Electrical)
   val common = input(Electrical)
   val sampleClock = input(Bool)
@@ -12,21 +13,24 @@ final class Adc(val width: Int = 12) extends Module:
   always(sampleClock.rising):
     code := toUInt(V(analogInput, common) / fullScale, width)
 
-final class Dac(val width: Int = 12) extends Module:
+final class Dac extends Module:
+  val width = param(12.integer)
   val code = input(UInt(width))
   val analogOutput = output(Electrical)
   val common = input(Electrical)
   val fullScale = param(1.0.V)
+  private val levels = (2.integer ** width) - 1.integer
 
   analog:
     V(analogOutput, common) <+ transition(
-      toReal(code) * fullScale / ((1 << width) - 1).real,
+      code.toReal * fullScale / levels.toReal,
       0.0.ns,
       1.0.ns,
       1.0.ns
     )
 
-final class MixedSignalHold(val width: Int = 10) extends Module:
+final class MixedSignalHold extends Module:
+  val width = param(10.integer)
   val analogInput = input(Electrical)
   val common = input(Electrical)
   val capture = input(Bool)
@@ -56,3 +60,20 @@ final class HierarchyAndOverride extends Module:
   connect(source, filter(_.source))
   connect(sink, filter(_.sink))
   connect(common, filter(_.common))
+
+final class ParameterizedAmsChain extends Module:
+  val width = param(10.integer)
+  val analogInput = inout(Electrical)
+  val analogOutput = inout(Electrical)
+  val common = inout(Electrical)
+  val sampleClock = input(Bool)
+
+  private val adc = instance(new Adc).param(_.width, width)
+  private val dac = instance(new Dac).param(_.width, width)
+
+  connect(analogInput, adc(_.analogInput))
+  connect(common, adc(_.common))
+  connect(sampleClock, adc(_.sampleClock))
+  connect(adc(_.code), dac(_.code))
+  connect(dac(_.analogOutput), analogOutput)
+  connect(dac(_.common), common)
